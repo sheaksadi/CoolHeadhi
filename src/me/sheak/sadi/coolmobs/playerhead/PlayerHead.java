51277912d7f9a274cfe2e162f9c76d346187ddbf
sheaksadi <sheaksadi123456@gmail.com>
2021-04-28 17:31:44 +0600

    fixed essue and xhanged to uuid

--- src/me/sheak/sadi/coolmobs/playerhead/PlayerHead.java
+++ src/me/sheak/sadi/coolmobs/playerhead/PlayerHead.java
@@ -14,17 +14,65 @@ public class PlayerHead {
         SkullMeta meta = (SkullMeta) item.getItemMeta();
 
         meta.setOwningPlayer(player);
-        if(player.getName().equalsIgnoreCase("sheaksadi"))
-            meta.setDisplayName(ChatColor.BLUE+""+ChatColor.BOLD+""+ player.getDisplayName() + "'s Head");
-        else
-            meta.setDisplayName(player.getDisplayName() + "'s Head");
 
+        ItemStack material=killer.getInventory().getItemInMainHand();
+        char[] sort= material.getType().toString().toCharArray();
+        StringBuilder builder=new StringBuilder();
+        boolean upperCase = true;
+        builder.append('[');
+        for(char s:sort){
+            if(s == '_') {
+                upperCase = true;
+                builder.append(' ');
+                continue;
+            }
+            if(upperCase) {
+                builder.append(Character.toUpperCase( s));
+                upperCase = false;
+            }
+            else
+            builder.append(Character.toLowerCase(s));
+
+        }
+        builder.append(']');
+        String killedItem=builder.toString();
+
+        if(player.getPlayer().getUniqueId().toString().equals("066d9c4e-18c5-4e68-924d-28139d18c07a")) {
+            meta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "" + player.getDisplayName() + "'s Head");
+        }
+        else {
+            meta.setDisplayName(ChatColor.YELLOW+""+ player.getDisplayName() + "'s Head");
+
+
+        }
         List<String> lore = new ArrayList<>();
         //esteregg code
-        if(killer.getName().equalsIgnoreCase("sheaksadi"))
+        try {
+            if (killer.getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) {
+                killedItem = "[" + killer.getInventory().getItemInMainHand().getItemMeta().getDisplayName() + "]";
+            }
+        }catch (Exception e){
+            killedItem ="Fist";
+        }
+
+
+        if(killer.getPlayer().getUniqueId().toString().equals("066d9c4e-18c5-4e68-924d-28139d18c07a")){
             lore.add(ChatColor.BOLD+""+ ChatColor.DARK_PURPLE+ "killed by The Great SheakSadi");
-        else
-            lore.add(ChatColor.GRAY+ "killed by "+killer.getDisplayName());
+
+            if(killedItem.equalsIgnoreCase("Fist"))
+                lore.add(ChatColor.BOLD+""+ ChatColor.DARK_PURPLE+ "With Fist" );
+            else
+                lore.add(ChatColor.BOLD+""+ ChatColor.DARK_PURPLE+ "Using " +ChatColor.AQUA+killedItem);
+
+        }
+
+        else {
+            lore.add(ChatColor.GRAY + "killed by " + killer.getDisplayName());
+            if(killedItem.equalsIgnoreCase("Fist"))
+                lore.add(ChatColor.GRAY+ "With Fist" );
+            else
+                lore.add(ChatColor.GRAY + "Using "+ChatColor.AQUA + killedItem);
+        }
 
         meta.setLore(lore);
         item.setItemMeta(meta);
